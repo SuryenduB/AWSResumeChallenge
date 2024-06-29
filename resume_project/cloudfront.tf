527e6dcf06ad041653a45c553f868d66c27b7abe
SuryenduB <suryendubbhattacharyya@gmail.com>
2024-06-29 12:56:26 +0200

    Change name of AWS CF Domain

--- resume_project/cloudfront.tf
+++ resume_project/cloudfront.tf
@@ -1,6 +1,6 @@
 locals {
   s3_origin_id   = "${var.s3_name}-origin"
-  s3_domain_name = "${var.s3_name}.s3-website-${var.region}.amazonaws.com"
+  s3_domain_name = "${var.s3_name}.s3-website.${var.region}.amazonaws.com"
 }
 
 resource "aws_cloudfront_distribution" "this" {
